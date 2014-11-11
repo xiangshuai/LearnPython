1351abf93e0c5ad9bbc670f1f1373b11a66dad4c
Kevin <yangjin1989@gmail.com>
2014-11-11 10:23:10 -0800

    1/2 chapter8

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,25 +1,25 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello, current_datetime, hours_ahead, display_meta
-from django.contrib import admin
-from books import views
-from contact.views import contact, thanks
-admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('mysite.views',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 
-    (r'^hello/$', hello),('^time/$', current_datetime),
-	(r'^another-time-page/$', current_datetime),
-	(r'^time/plus/(\d{1,2})/$', hours_ahead),
-	(r'^meta/$', display_meta),
-	(r'^search-form/$', views.search_form),
-	(r'^search/$', views.search),
-	(r'^contact/$', contact),
-	(r'^contact/thanks/',thanks),
-	
-
+    (r'^hello/$', 'hello'),('^time/$', 'current_datetime'),
+	(r'^another-time-page/$', 'current_datetime'),
+	(r'^time/plus/(\d{1,2})/$', 'hours_ahead'),
+	(r'^meta/$', 'display_meta'),
 )
+
+urlpatterns += patterns('books.views',
+	(r'^search-form/$', 'search_form'),
+	(r'^search/$', 'search'),
+)
+
+urlpatterns += patterns('contact.views',
+	(r'^contact/$', 'contact'),
+	(r'^contact/thanks/','thanks'),
+)	
+	
--- django/mysite/mysite/views.py
+++ django/mysite/mysite/views.py
@@ -18,7 +18,7 @@ def hours_ahead(request, offset):
 	except ValueError:
 		raise Http404()
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	assert False
+	#assert False
 	html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
 	return HttpResponse(html)
 
